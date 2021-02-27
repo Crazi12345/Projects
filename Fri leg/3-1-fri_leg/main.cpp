0d815eec0e8361e6f8bd9e9dc1cb92cfe46ed9d8
Crazi12345 <patrickbjerre@outlook.dk>
2021-02-27 22:00:41 +0100

    halv færdig

--- Fri leg/3-1-fri_leg/main.cpp
+++ Fri leg/3-1-fri_leg/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 
-using namespace std;
+
+class ClassName
+{
+
+public:
+  void print(std::string name){
+    std::cout << name << std::endl;
+  }
+  void print(int name){
+    std::cout << name-1 << std::endl;
+  }
+  void loop(){
+    for (int i =0; i<10; i++){
+        std::cout<< i << std::endl;
+      }
+  }
+  int increment(int& x){
+
+    x++;
+    return x;
+  }
+
+private:
+  std::string name;
+
+};
 
 int main()
 {
-  cout << "Hello World!" << endl;
+  ClassName object;
+  int  i= 0;
+  std::cout <<"i: "<<i<< std::endl;
+  object.print("I AM VERY TIRED");
+  object.print(4);
+  object.loop();
+  object.increment(i);
+  std::cout <<"i: "<<i<<std::endl;
+
   return 0;
 }
